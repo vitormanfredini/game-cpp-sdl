c2000a62cd30d021f2fffdd42061b378fa299f69
Vítor Manfredini <vitormanfredini@gmail.com>
2025-02-20 16:56:16 -0300

    refactored Camera to instantiate averages in constructor

--- src/Camera.h
+++ src/Camera.h
@@ -6,30 +6,23 @@ class Camera {
 
 private:
     int samplesAverage;
-    MovingAverage* averagePosX;
-    MovingAverage* averagePosY;
+    MovingAverage averagePosX;
+    MovingAverage averagePosY;
     float averagePosXCache = 0.0f;
     float averagePosYCache = 0.0f;
 
 public:
 
-    Camera(int samples, float startX, float startY): samplesAverage(samples) {
-        averagePosX = new MovingAverage(samples);
-        averagePosX->fill(startX);
-        averagePosY = new MovingAverage(samples);
-        averagePosY->fill(startY);
-    }
-
-    ~Camera(){
-        delete averagePosX;
-        delete averagePosY;
+    Camera(int samples, float startX, float startY): samplesAverage(samples), averagePosX(samples), averagePosY(samples) {
+        averagePosX.fill(startX);
+        averagePosY.fill(startY);
     }
 
     void update(float positionX, float positionY){
-        averagePosX->addValue(positionX);
-        averagePosY->addValue(positionY);
-        averagePosXCache = averagePosX->getAverage();
-        averagePosYCache = averagePosY->getAverage();
+        averagePosX.addValue(positionX);
+        averagePosY.addValue(positionY);
+        averagePosXCache = averagePosX.getAverage();
+        averagePosYCache = averagePosY.getAverage();
     }
 
     float getPositionX(){
