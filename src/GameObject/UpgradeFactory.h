3dc7b9c6a340f536a1160ee9cf4380cdcccf9967
Vítor Manfredini <vitormanfredini@gmail.com>
2025-04-17 00:15:30 -0300

    minor refactor

--- src/GameObject/UpgradeFactory.h
+++ src/GameObject/UpgradeFactory.h
@@ -17,34 +17,38 @@ public:
 
     UpgradeFactory(){
 
-        upgradesLastIndexesUsed[UpgradeId::MaxHealth] = 0;
         prototypes[UpgradeId::MaxHealth].push_back(std::make_unique<UpgradeComponent>(UpgradeId::MaxHealth, StatType::MaxHealth, 1, 0.2));
         prototypes[UpgradeId::MaxHealth].push_back(std::make_unique<UpgradeComponent>(UpgradeId::MaxHealth, StatType::MaxHealth, 2, 0.1));
         prototypes[UpgradeId::MaxHealth].push_back(std::make_unique<UpgradeComponent>(UpgradeId::MaxHealth, StatType::MaxHealth, 3, 0.08));
 
-        upgradesLastIndexesUsed[UpgradeId::BaseSpeed] = 0;
+        
         prototypes[UpgradeId::BaseSpeed].push_back(std::make_unique<UpgradeComponent>(UpgradeId::BaseSpeed, StatType::BaseSpeed, 1, 0.2));
         prototypes[UpgradeId::BaseSpeed].push_back(std::make_unique<UpgradeComponent>(UpgradeId::BaseSpeed, StatType::BaseSpeed, 2, 0.1));
         prototypes[UpgradeId::BaseSpeed].push_back(std::make_unique<UpgradeComponent>(UpgradeId::BaseSpeed, StatType::BaseSpeed, 3, 0.08));
 
-        availableUpgrades = {
+        availableUpgradeIds = {
             UpgradeId::MaxHealth,
             UpgradeId::BaseSpeed
         };
+
+        for(UpgradeId upgradeId : availableUpgradeIds){
+            upgradesTimesConsumed[upgradeId] = 0;
+        }
+
     }
 
     std::vector<std::unique_ptr<UpgradeComponent>> createRandomUpgrades(int max){
         auto rng = std::default_random_engine {};
-        std::shuffle(std::begin(availableUpgrades), std::end(availableUpgrades), rng);
+        std::shuffle(std::begin(availableUpgradeIds), std::end(availableUpgradeIds), rng);
 
-        std::vector<UpgradeId> limitedAvailableUpgradeIds = availableUpgrades;
+        std::vector<UpgradeId> limitedAvailableUpgradeIds = availableUpgradeIds;
         if (limitedAvailableUpgradeIds.size() > max) {
             limitedAvailableUpgradeIds.erase(limitedAvailableUpgradeIds.begin() + max, limitedAvailableUpgradeIds.end());
         }
 
         std::vector<std::unique_ptr<UpgradeComponent>> randomUpgrades = {};
         for(UpgradeId upgradeId : limitedAvailableUpgradeIds){
-            randomUpgrades.push_back(std::move(prototypes[upgradeId][upgradesLastIndexesUsed[upgradeId]]->clone()));
+            randomUpgrades.push_back(std::move(prototypes[upgradeId][upgradesTimesConsumed[upgradeId]]->clone()));
         }
 
         return randomUpgrades;
@@ -52,24 +56,21 @@ public:
 
     void playerChoseThisUpgrade(UpgradeComponent* upgrade){
         UpgradeId upgradeId = upgrade->getId();
-        upgradesLastIndexesUsed[upgradeId] += 1;
+        upgradesTimesConsumed[upgradeId] += 1;
 
-        if(upgradesLastIndexesUsed[upgradeId] >= prototypes[upgradeId].size()){
-            availableUpgrades.erase(std::remove(availableUpgrades.begin(), availableUpgrades.end(), upgradeId), availableUpgrades.end());
+        if(upgradesTimesConsumed[upgradeId] >= prototypes[upgradeId].size()){
+            availableUpgradeIds.erase(std::remove(availableUpgradeIds.begin(), availableUpgradeIds.end(), upgradeId), availableUpgradeIds.end());
         }
     }
 
 private:
     std::unordered_map<UpgradeId,std::vector<std::unique_ptr<UpgradeComponent>>> prototypes;
-    std::unordered_map<UpgradeId,int> upgradesLastIndexesUsed;
-    std::vector<UpgradeId> availableUpgrades;
+    std::unordered_map<UpgradeId,int> upgradesTimesConsumed;
+    std::vector<UpgradeId> availableUpgradeIds;
 
     std::unique_ptr<UpgradeComponent> create(UpgradeId upgradeId) {
-        upgradesLastIndexesUsed[upgradeId] += 1;
-        std::unique_ptr<UpgradeComponent> newUpgrade = prototypes[upgradeId][upgradesLastIndexesUsed[upgradeId]-1]->clone();
-
-        
-
+        upgradesTimesConsumed[upgradeId] += 1;
+        std::unique_ptr<UpgradeComponent> newUpgrade = prototypes[upgradeId][upgradesTimesConsumed[upgradeId]-1]->clone();
         return newUpgrade;
     }
 
