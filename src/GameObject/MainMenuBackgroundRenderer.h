c3b91190f598224c9dd80d8d0d9f9f61cb5cb4cd
Vítor Manfredini <vitormanfredini@gmail.com>
2025-10-06 21:35:07 -0300

    main menu animation

--- src/GameObject/MainMenuBackgroundRenderer.h
+++ src/GameObject/MainMenuBackgroundRenderer.h
@@ -55,7 +55,6 @@ public:
         if(sweepAnimation < numberColumns * 4){
             int xSweep = sweepAnimation / 2;
             int ySweep = 0;
-            bool finished = false;
             while(xSweep < numbers[0].size() * 2){
                 if(ySweep < numbers.size() && xSweep < numbers[0].size()){
                     numbers[ySweep][xSweep] = RandomGenerator::getInstance().getRandom() > 0.5 ? 1 : 0;
